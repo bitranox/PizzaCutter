c92bde151076706221bc1b623e2a103936d1b76f
bitranox <rnowotny1966@gmail.com>
2020-08-01 16:00:34 +0200

    refractor

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ if is_travis_deploy() and is_tagged_commit():
 
 setup_kwargs: Dict[str, Any] = dict()
 setup_kwargs['name'] = 'pizzacutter'
-setup_kwargs['version'] = 'v1.1.6'
+setup_kwargs['version'] = 'v1.1.7'
 setup_kwargs['url'] = 'https://github.com/bitranox/PizzaCutter'
 setup_kwargs['packages'] = find_packages()
 setup_kwargs['package_data'] = {'pizzacutter': ['py.typed', '*.pyi', '__init__.pyi']}
