99bf0f341b987a1288f79ff5472969b839fcf11f
bitranox <rnowotny1966@gmail.com>
2021-11-16 23:58:34 +0100

    refractor

--- pizzacutter/pizzacutter.py
+++ pizzacutter/pizzacutter.py
@@ -10,12 +10,14 @@ import pathlib3x as pathlib
 try:
     from .sub import get_config
     from .sub import helpers
+    from .sub.helpers import find_version_number_in_file
     from .sub import import_module
     from .sub.pizzacutter_config import PizzaCutterConfigBase
 except (ImportError, ModuleNotFoundError):  # pragma: no cover
     # imports for doctest
     from sub import get_config  # type: ignore  # pragma: no cover
     from sub import helpers  # type: ignore  # pragma: no cover
+    from sub.helpers import find_version_number_in_file
     from sub import import_module  # type: ignore  # pragma: no cover
     from sub.pizzacutter_config import PizzaCutterConfigBase  # type: ignore  # pragma: no cover
 
